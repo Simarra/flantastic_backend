dba98436dbf7f35ee6a8e44e32dc9a18f4034656
loicm <loicm@localhost.localdomain>
2020-12-23 22:04:23 +0100

    init (all): manage version blueprint

--- flantastic_backend/flantastic_backend/app.py
+++ flantastic_backend/flantastic_backend/app.py
@@ -2,4 +2,5 @@ from flask import Flask
 from flantastic_backend.flantastic_backend.api.v1.routes import flantastic_bp
 
 flantastic_app = Flask(__name__)
+flantastic_app.register_blueprint(flantastic_bp, url_prefix="")
 flantastic_app.register_blueprint(flantastic_bp)
